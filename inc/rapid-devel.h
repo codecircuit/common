ab6d42f9a23e6f06f94c2b1adff9f58e48430a4c
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2017-11-14 18:14:54 +0100

    added cuda error check macro

--- inc/rapid-devel.h
+++ inc/rapid-devel.h
@@ -42,3 +42,13 @@ std::string getdate() {
 	strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
 	return buf;
 }
+
+#define checkCudaError(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
